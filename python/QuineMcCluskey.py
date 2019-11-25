4d19533f7c28960b14146dd4b8e0423ddab8d251
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2019-11-25 14:01:40 +0100

    mc

--- python/QuineMcCluskey.py
+++ python/QuineMcCluskey.py
@@ -1,25 +1,30 @@
 def dec_to_bin(n):
     return bin(n).replace('0b', '')
 
+def main():
+    vars = int(input('Inserire il numero delle variabili: '));
 
-n_vars = input('Inserire il numero delle variabili: ');
+    minterms = [int(x) for x in input('inserire i minterms: ').split()]
+    minterms.sort()
 
-vars = []
+    impl_primi, impl_ess, finzioni = tabella(vars, minterms)
+    minterms_categorised = categorize(minterms,vars)
 
-i = 0
-while 0 <= i <= 2 ** int(n_vars) - 1:
-    bits = input(F'Inserire il minterm {i+1}: ')
-    vars.append(dec_to_bin(int(bits)).zfill(int(n_vars)))
+def tabella(vars, minterms):
+    impl_primi = []
+    funzioni = []
 
-    i += 1
+    minterms = [bin(int(x))[2:].zfill(vars) for x in minterms]
 
-vars.sort()
+    vars.sort()
 
-for x in range(2 ** int(n_vars) - 1):
-    count = vars[x].count('1')
+def categorize(minterms,vars):
+    minterms_categorised = {}
 
-    if count == 0:
-        vars_first = []
+	for i in range(vars + 1):
+		minterms_categorised[i] = []
 
-print(n_vars)
-print(vars)
+	for i in minterms:
+		minterms_categorised[i.count('1')].append([i, [int(i, 2)]])
+
+	return minterms_categorised
