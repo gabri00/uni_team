3634e261dc91fdc79d9aca3c52c5d7baeb19b32b
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2019-11-26 14:46:58 +0100

    fix

--- cpp/cicle/cicle/Origine.cpp
+++ cpp/cicle/cicle/Origine.cpp
@@ -3,29 +3,41 @@
 
 using namespace std;
 
+#define PI	3.14
+
 int main()
 {
 	float r;
-	const float PI = 3.14f;
 	char sel;
 
 	cout << "Before doing anything enter the length of the radius: ";
-	cin >> r;
+	cin >> dec >> r;
 
 	cout << "What do you want to calculate, area(a) or perimeter(p) or both(b)?";
 	cin >> sel;
 
-	float area = PI * pow(r, 2.0);
-	float per = 2 * PI * r;
+	const float area = PI * pow(r, 2.0),
+				per = 2 * PI * r;
 
-	if (sel == 'a')
-		cout << "The area of the circle is: " << area << endl;
-	else if (sel == 'p')
-		cout << "The perimeter of the circle is: " << per << endl;
-	else if (sel == 'b')
-		cout << "The area is: " << area << " and the perimeter is: " << per << endl;
-	else
-		cout << sel << " is not a valid option you asshole" << endl;
+
+	switch (sel)
+	{
+	case 'a':
+		cout << "The area of the circle is: " << area << '\n';
+		break;
+
+	case 'p':
+		cout << "The perimeter of the circle is: " << per << '\n';
+		break;
+
+	case 'b':
+		cout << "The area is: " << area << " and the perimeter is: " << per << '\n';
+		break;
+
+	default:
+		cout << sel << " is not a valid option you asshole\n";
+		break;
+	}
 
 
 	char answer;
@@ -34,18 +46,18 @@ int main()
 	cout << "Would you like to calculate the area and perimeter of the inscribed square (y)/(n)? ";
 	cin >> answer;
 
-	if (answer == 'n' || answer == 'N')		cout << "Okay, bye bitch." << endl;
+	if (answer == 'n' || answer == 'N')		cout << "Okay, bye bitch.\n";
 	else if (answer == 'y' || answer == 'Y')
 	{
-		cout << "Nice, I'll calculate them for you..." << endl;
+		cout << "Nice, I'll calculate them for you...\n";
 
-		float l = sqrt(pow(r, 2.0) + pow(r, 2.0));
-		float sq_area = pow(l, 2.0);
-		float sq_per = l * 4;
+		const float l = sqrt(pow(r, 2.0) + pow(r, 2.0)),
+					sq_area = pow(l, 2.0),
+					sq_per = l * 4;
 
-		cout << "The area of the square is: " << sq_area << " and the perimeter: " << sq_per << endl;
+		cout << "The area of the square is: " << sq_area << " and the perimeter: " << sq_per << '\n';
 	}
-
+	else	cout << "Not valid expression!\n";
 
 	return 0;
 }
