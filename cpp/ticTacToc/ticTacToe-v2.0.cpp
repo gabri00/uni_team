a3f17ba77b5ba07a3792a041282a1a57c7d8ca7d
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2019-12-02 14:15:36 +0100

    perfezionamenti

--- cpp/ticTacToc/ticTacToe-v2.0.cpp
+++ cpp/ticTacToc/ticTacToe-v2.0.cpp
@@ -1,206 +1,228 @@
 #include <iostream>
+//#include <limits>
 
 using namespace std;
 
-char Tabella[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' };
-char player = 'X';
+#define HEADER	"\t***\tTRIS GAME DEVELOPED BY LORENZO MONTANARO\t***\n"
+
+char Tabella[3][3] = { '1', '2', '3', '4', '5', '6', '7', '8', '9' },
+player = 'X';
+
+
+/*int checkCin(int a)
+{
+	while (cin.fail())
+	{
+		cin.clear();
+		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+		cout << "Input non valido, riprovare: ";
+		cin >> a;
+	}
+}*/
 
 void Disegno()
 {
-    cout << "Tris\n";
+	cout << "\n\n\n";
 
-    for (int row = 0; row < 3; row++)
-    {
-        for (int col = 0; col < 3; col++)
-        {
-            cout << Tabella[row][col] << ' ';
-        }
-        cout << '\n';
-    }
+	for (int row = 0; row < 3; row++)
+	{
+		for (int col = 0; col < 3; col++)
+		{
+			cout << Tabella[row][col] << '\t';
+		}
+		cout << "\n\n\n";
+	}
 }
 
 void Input()
 {
-    int a;
-    cout << "E' il turno di " << player <<". Inserire il numero della tabella da sostituire: ";
-    cin >> a;
+	int a;
 
-    if (a == 1)
-    {
-        if (Tabella[0][0] == '1')
-            Tabella[0][0] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    else if (a == 2)
-    {
-        if (Tabella[0][1] == '2')
-            Tabella[0][1] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    else if (a == 3)
-    {
-        if (Tabella[0][2] == '3')
-            Tabella[0][2] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    else if (a == 4)
-    {
-        if (Tabella[1][0] == '4')
-            Tabella[1][0] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    else if (a == 5)
-    {
-        if (Tabella[1][1] == '5')
-            Tabella[1][1] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    else if (a == 6)
-    {
-        if (Tabella[1][2] == '6')
-            Tabella[1][2] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    else if (a == 7)
-    {
-        if (Tabella[2][0] == '7')
-            Tabella[2][0] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    else if (a == 8)
-    {
-        if (Tabella[2][1] == '8')
-            Tabella[2][1] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    else if (a == 9)
-    {
-        if (Tabella[2][2] == '9')
-            Tabella[2][2] = player;
-        else
-        {
-            cout << "Casella già occupata, riprova!" << endl;
-            Input();
-        }
-    }
-    
+	cout << "E' il turno di " << player << ".\nInserire il numero della tabella da sostituire: ";
+	cin >> a;
+
+	//a = checkCin(a);
+
+	switch (a)
+	{
+	case 1:
+		if (Tabella[0][0] == '1')	Tabella[0][0] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+
+	case 2:
+		if (Tabella[0][1] == '2')	Tabella[0][1] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+
+	case 3:
+		if (Tabella[0][2] == '3')	Tabella[0][2] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+
+	case 4:
+		if (Tabella[1][0] == '4')	Tabella[1][0] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+
+	case 5:
+		if (Tabella[1][1] == '5')	Tabella[1][1] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+
+	case 6:
+		if (Tabella[1][2] == '6')	Tabella[1][2] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+
+	case 7:
+		if (Tabella[2][0] == '7')	Tabella[2][0] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+
+	case 8:
+		if (Tabella[2][1] == '8')	Tabella[2][1] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+
+	case 9:
+		if (Tabella[2][2] == '9')	Tabella[2][2] = player;
+		else
+		{
+			cout << "Casella già occupata, riprova!\n";
+			Input();
+		}
+
+		break;
+	}
 }
 
 void SwitchPlayer()
 {
-    if (player == 'X')
-        player = 'O';
-    else
-        player = 'X';
+	(player == 'X') ? player = 'O' : player = 'X';
 }
 
 char WinCheck()
 {
-    
-    if (Tabella[0][0] == 'X' && Tabella[0][1] == 'X' && Tabella[0][2] == 'X')
-        return 'X';
-    if (Tabella[1][0] == 'X' && Tabella[1][1] == 'X' && Tabella[1][2] == 'X')
-        return 'X';
-    if (Tabella[2][0] == 'X' && Tabella[2][1] == 'X' && Tabella[2][2] == 'X')
-        return 'X';
 
-    if (Tabella[0][0] == 'X' && Tabella[1][0] == 'X' && Tabella[2][0] == 'X')
-        return 'X';
-    if (Tabella[0][1] == 'X' && Tabella[1][1] == 'X' && Tabella[2][1] == 'X')
-        return 'X';
-    if (Tabella[0][2] == 'X' && Tabella[1][2] == 'X' && Tabella[2][2] == 'X')
-        return 'X';
+	if (Tabella[0][0] == 'X' && Tabella[0][1] == 'X' && Tabella[0][2] == 'X')
+		return 'X';
+	else if (Tabella[1][0] == 'X' && Tabella[1][1] == 'X' && Tabella[1][2] == 'X')
+		return 'X';
+	else if (Tabella[2][0] == 'X' && Tabella[2][1] == 'X' && Tabella[2][2] == 'X')
+		return 'X';
 
-    if (Tabella[0][0] == 'X' && Tabella[1][1] == 'X' && Tabella[2][2] == 'X')
-        return 'X';
-    if (Tabella[2][0] == 'X' && Tabella[1][1] == 'X' && Tabella[0][2] == 'X')
-        return 'X';
+	else if (Tabella[0][0] == 'X' && Tabella[1][0] == 'X' && Tabella[2][0] == 'X')
+		return 'X';
+	else if (Tabella[0][1] == 'X' && Tabella[1][1] == 'X' && Tabella[2][1] == 'X')
+		return 'X';
+	else if (Tabella[0][2] == 'X' && Tabella[1][2] == 'X' && Tabella[2][2] == 'X')
+		return 'X';
 
-    
-    if (Tabella[0][0] == 'O' && Tabella[0][1] == 'O' && Tabella[0][2] == 'O')
-        return 'O';
-    if (Tabella[1][0] == 'O' && Tabella[1][1] == 'O' && Tabella[1][2] == 'O')
-        return 'O';
-    if (Tabella[2][0] == 'O' && Tabella[2][1] == 'O' && Tabella[2][2] == 'O')
-        return 'O';
+	else if (Tabella[0][0] == 'X' && Tabella[1][1] == 'X' && Tabella[2][2] == 'X')
+		return 'X';
+	else if (Tabella[2][0] == 'X' && Tabella[1][1] == 'X' && Tabella[0][2] == 'X')
+		return 'X';
 
-    if (Tabella[0][0] == 'O' && Tabella[1][0] == 'O' && Tabella[2][0] == 'O')
-        return 'O';
-    if (Tabella[0][1] == 'O' && Tabella[1][1] == 'O' && Tabella[2][1] == 'O')
-        return 'O';
-    if (Tabella[0][2] == 'O' && Tabella[1][2] == 'O' && Tabella[2][2] == 'O')
-        return 'O';
 
-    if (Tabella[0][0] == 'O' && Tabella[1][1] == 'O' && Tabella[2][2] == 'O')
-        return 'O';
-    if (Tabella[2][0] == 'O' && Tabella[1][1] == 'O' && Tabella[0][2] == 'O')
-        return 'O';
+	else if (Tabella[0][0] == 'O' && Tabella[0][1] == 'O' && Tabella[0][2] == 'O')
+		return 'O';
+	else if (Tabella[1][0] == 'O' && Tabella[1][1] == 'O' && Tabella[1][2] == 'O')
+		return 'O';
+	else if (Tabella[2][0] == 'O' && Tabella[2][1] == 'O' && Tabella[2][2] == 'O')
+		return 'O';
 
-    return '/';
+	else if (Tabella[0][0] == 'O' && Tabella[1][0] == 'O' && Tabella[2][0] == 'O')
+		return 'O';
+	else if (Tabella[0][1] == 'O' && Tabella[1][1] == 'O' && Tabella[2][1] == 'O')
+		return 'O';
+	else if (Tabella[0][2] == 'O' && Tabella[1][2] == 'O' && Tabella[2][2] == 'O')
+		return 'O';
+
+	else if (Tabella[0][0] == 'O' && Tabella[1][1] == 'O' && Tabella[2][2] == 'O')
+		return 'O';
+	else if (Tabella[2][0] == 'O' && Tabella[1][1] == 'O' && Tabella[0][2] == 'O')
+		return 'O';
+
+	return '/';
 }
 
 int main()
 {
-    int n = 0;
+	cout << HEADER;
+	int n = 0;
 
-    Disegno();
+	Disegno();
 
-    while (1)
-    {
-        n++;
-        Input();
+	while (true)
+	{
+		n++;
 
-        Disegno();
+		Input();
 
-        if (WinCheck() == 'X')
-        {
-            cout << "La X vince!" << endl;
-            break;
-        }
-        else if (WinCheck() == 'O')
-        {
-            cout << "Il O vince!" << endl;
-            break;
-        }
-        else if (WinCheck() == '/' && n == 9)
-        {
-            cout << "Pareggio" << endl;
-            break;
-        }
-        SwitchPlayer();
-    }
-    
-    return 0;
+		Disegno();
+
+		if (WinCheck() == 'X')
+		{
+			cout << "La X vince!\n";
+			break;
+		}
+		else if (WinCheck() == 'O')
+		{
+			cout << "Il O vince!\n";
+			break;
+		}
+		else if (WinCheck() == '/' && n == 9)
+		{
+			cout << "Pareggio!\n";
+			break;
+		}
+
+		SwitchPlayer();
+	}
+
+	cout << "Partita terminata\n";
+
+	return 0;
 }
