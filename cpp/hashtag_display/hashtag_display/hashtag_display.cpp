8033008936479552ab7348c593e121a0638816f2
Gabriele Nicchiarelli <31885249+gabri00@users.noreply.github.com>
2019-12-21 22:04:48 +0100

    mod simpatica

--- cpp/hashtag_display/hashtag_display/hashtag_display.cpp
+++ cpp/hashtag_display/hashtag_display/hashtag_display.cpp
@@ -1,14 +1,31 @@
 #include <iostream>
 #include <limits>
-
-using namespace std;
+#include <random>
 
 #define HASHTAG	'#'
 #define HEADER	"*****\tWELCOME TO THE HASHTAG PARTY\t*****\n\n"
 
+void print_hashtags(int n, int max_raw)
+{
+	int color = 0;
+
+	for (int y = 1; y <= n; y++)
+	{
+		std::uniform_int_distribution<int> rand = std::uniform_int_distribution<int>(31, 36);
+		std::mt19937 rng = std::mt19937(std::random_device{}());
+		color = rand(rng);
+
+		std::cout << "\033[0;" << color << "m" << HASHTAG << "\033[0m" << ' ';
+
+		if (y % max_raw == 0)
+			std::cout.put('\n');
+	}
+}
 
 int main()
 {
+	using namespace std;
+
 	cout << HEADER;
 
 	int n, max_raw;
@@ -42,19 +59,12 @@ int main()
 	while (max_raw == 0)
 	{
 		cout << "at least one element per raw, enter a valid number: ";
-		cin.sync();
 		cin.clear();
 		cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 		cin >> dec >> max_raw;
 	}
 
-	for (int y = 1; y < n + 1; y++)
-	{
-		cout << HASHTAG << ' ';
-
-		if (y % max_raw == 0)	cout.put('\n');
-
-	}
+	print_hashtags(n, max_raw);
 
 	return 0;
 }
